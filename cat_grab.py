fb0dd2c6072c2fb4f3674b1cd4d59e38a6f3a7fe
Nick Beede <nbeede7@gmail.com>
2014-01-25 20:23:38 -0500

    Added example cat_list and first commit of working code + usage

--- cat_grab.py
+++ cat_grab.py
@@ -1,15 +1,34 @@
 """
 cat_grab.py
-    Downloads all images from a specified URL.
+    Downloads all images from a text file of line delimeted URLs.
+    
+    Usage: python cat_grab.py cat_list.txt
 """
 from bs4 import BeautifulSoup as bs
-import urlparse
+import urlparse, os, sys
 from urllib2 import urlopen
 from urllib import urlretrieve
-import os
-import sys
 
-def main(ur;, out_folder="/Users/nbeede7/Cats"):
-    """Downloads all the images at 'url' to /Users/nbeede7/Cats"""
-    soup = bs(urlopen(url))
-    parsed = list(urlparse.urlparse(url))
+def main(url_file, out_folder="/Users/nbeede7/Cats"):
+    """Downloads all the images at 'url' to /cats/"""
+    with open(url_file) as u_file:
+        url_list = u_file.readlines()
+        
+    for url in url_list:
+        soup = bs(urlopen(url))
+        url_parsed = list(urlparse.urlparse(url))
+        
+        for image in soup.findAll("img"):
+            print "Image: " + (image["src"])
+            filename = image["src"].split("/")[-1]
+            url_parsed[2] = image["src"]
+            outpath = os.path.join(out_folder, filename)
+            if image["src"].lower().startswith("http"):
+                urlretrieve(image["src"], outpath)
+            else:
+                urlretrieve(urlparse.urlunparse(parsed), outpath)
+    
+if __name__ == "__main__":
+    url_file = sys.argv[-1]
+    out_folder = "/Users/nbeede7/Cats"
+    main(url_file, out_folder)
