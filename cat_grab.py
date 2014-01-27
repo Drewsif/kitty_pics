773cb98fec252e527d468309cd21030063710d81
Drew Bonasera <dbonasera@mitre.org>
2014-01-27 18:44:00 -0500

    cleaned up some extra code

--- cat_grab.py
+++ cat_grab.py
@@ -12,18 +12,6 @@ import urllib2
 from urllib import urlretrieve
 import re
 import time
-from random import choice
-
-user_agents = [
-    'Mozilla/5.0 (Windows; U; Windows NT 5.1; it; rv:1.8.1.11) Gecko/20071127 Firefox/2.0.0.11',
-    'Opera/9.25 (Windows NT 5.1; U; en)',
-    'Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.1; SV1; .NET CLR 1.1.4322; .NET CLR 2.0.50727)',
-    'Mozilla/5.0 (compatible; Konqueror/3.5; Linux) KHTML/3.5.5 (like Gecko) (Kubuntu)',
-    'Mozilla/5.0 (Windows NT 5.1) AppleWebKit/535.19 (KHTML, like Gecko) Chrome/18.0.1025.142 Safari/535.19',
-    'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.7; rv:11.0) Gecko/20100101 Firefox/11.0',
-    'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.6; rv:8.0.1) Gecko/20100101 Firefox/8.0.1',
-    'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/535.19 (KHTML, like Gecko) Chrome/18.0.1025.151 Safari/535.19'
-]
 
 def download(url, out_folder):
     filename = url.split("/")[-1]
@@ -58,7 +46,6 @@ def redditurl(url, out_folder, max_number):
         except:    
             return(-2)
         print next_link
-        version = choice(user_agents)
         req = urllib2.Request(next_link, None, headers)
         htmlText = urllib2.urlopen(req).read()
         soup = bs(htmlText)
